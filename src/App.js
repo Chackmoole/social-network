3e66ffcd226817e8aa2aaf91e7cf0179d9ed00de
Chackmoole <cebep-21@mail.ru>
2022-08-24 18:54:31 +0300

    Урок 19 Добавлен пакет react-router-dom и настроен первоначальный роутинг

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Setting/Setting";
 
 const App = () => {
   return (
-    <div className="app__wrapper">
-      <Header />
-      <NavBar />
-      <div className="app__inner">
-        <Profile />
+    <BrowserRouter>
+      <div className="app__wrapper">
+        <Header />
+        <NavBar />
+        <div className="app__inner">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,19 +4,29 @@ const NavBar = () => {
   return (
     <nav className={styles.nav}>
       <div>
-        <a className={styles.link}>Propfiles</a>
+        <a className={styles.link} href="/profile">
+          Profiles
+        </a>
       </div>
       <div>
-        <a className={styles.link}>Message</a>
+        <a className={styles.link} href="/dialogs">
+          Message
+        </a>
       </div>
       <div>
-        <a className={styles.link}>News</a>
+        <a className={styles.link} href="/news">
+          News
+        </a>
       </div>
       <div>
-        <a className={styles.link}>Music</a>
+        <a className={styles.link} href="/music">
+          Music
+        </a>
       </div>
       <div>
-        <a className={styles.link}>Settings</a>
+        <a className={styles.link} href="/settings">
+          Settings
+        </a>
       </div>
     </nav>
   );
