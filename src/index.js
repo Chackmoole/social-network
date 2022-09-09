55b5ea26afcaab0784046f0168abe799efe206ff
Chackmoole <cebep-21@mail.ru>
2022-09-09 11:19:53 +0300

    Урок 35 убрали render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+import React from "react";
+import App from "./App";
 import "./index.css";
-import state from "./redux/state";
-import { renderEntireTree } from "./render";
+import state, {
+  addPost,
+  addProfileMessage,
+  subscribe,
+  updateDialogMessageText,
+  updateProfileMessageText,
+} from "./redux/state";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        updateProfileMessageText={updateProfileMessageText}
+        addPost={addPost}
+        addProfileMessage={addProfileMessage}
+        updateDialogMessageText={updateDialogMessageText}
+      />
+    </React.StrictMode>
+  );
+};
 
 renderEntireTree(state);
+
+subscribe(renderEntireTree);
